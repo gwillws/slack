3f2d5b0e4af55e5116c009464ad4935ba50aae6f
imalbert <albert.s.manuel@gmail.com>
2015-09-14 17:06:23 +0800

    Type in and display messages, sidemenu working

---
+++ client/components/messages.js
@@ -0,0 +1,10 @@
+Template.messages.onCreated(function() {
+  this.subscribe('messages');
+  this.subscribe('allUsernames');
+});
+
+Template.messages.helpers({
+  messages: function() {
+    return Messages.find();
+  }
+})
